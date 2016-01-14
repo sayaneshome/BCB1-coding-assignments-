c42a21ef204c4deb827470f08fb04b064ee7fcb2
sayane shome <sayane.shome2009@vit.ac.in>
2016-01-14 06:35:54 -0600

    removing comment line

---
+++ Approx_patternmatch.py
@@ -0,0 +1,141 @@
+#Please install Python 2.7.10 and Numpy 1.10-1.Please make sure Numpy isnt installed in working directly.
+#Program for approximate pattern matching upto k mismatches.
+
+import numpy
+import array   
+
+def readSeq(FASTA): #read the sequence
+    for line in FASTA:
+        if line.startswith('>'):
+            continue
+        line = line.strip()
+        return line
+
+
+def posList(pattern, text): #determine the positions in the matrix
+    retList = list()
+    textList = [x for x in text.strip()]
+    for i, char1 in enumerate(pattern):
+        textPos = [j for j, char2 in enumerate(textList) if char1==char2]
+        for j in textPos:
+            retList.append((i+1,j+1))
+    return retList
+
+def cost_match(i,j,M):
+        return min(M[i,j-1]+1, M[i-1,j-1], M[i-1,j]+1)
+
+def cost_mismatch(i,j,M):
+        return min(M[i,j-1]+1, M[i-1,j]+1, M[i-1,j-1]+1)
+ 
+
+
+def fill(M,text,compare, pattern):
+    n = len(text)
+    m = len(pattern)
+    positions = posList(pattern,text)
+    print positions
+    
+    for i in range(1,m+1):
+        for j in range(1,n+1):
+            if (i,j) in positions:
+                M[i,j] = cost_match(i,j,M)
+            else:
+                M[i,j] = cost_mismatch(i,j,M)
+
+    return M
+
+                    
+def trace(M,pattern,text,k):
+    list=[]
+    n = len(text)
+    m = len(pattern)
+    i=m
+    j=n
+    positions = posList(pattern,text)
+    
+    tempi=m
+    tempj=n
+    while(j!=-1):
+        if i==m:
+           tempj=j
+        if i==0:
+            if j+1 not in list:
+                list.append(j+1)
+            i=m
+            j=tempj-1
+            tempj=j
+        if M[i][j]<=k:
+            if (i,j) not in positions:
+                if M[i-1][j-1]+1<=min(M[i-1][j]+1,M[i][j-1]+1) and M[i-1][j-1]<=k:
+                    
+                    i=i-1#Moving diagonally
+                    j=j-1
+    
+                elif M[i][j-1]+1<=min(M[i-1][j-1]+1,M[i-1][j]+1) and M[i][j-1]<=k:  
+                    
+                    j=j-1#Moving horizontally
+                elif M[i-1][j]+1<=min(M[i-1][j-1]+1,M[i][j-1]+1) and M[i-1][j]<=k:
+                    
+                    i=i-1#Moving vertically
+            elif (i,j) in positions:
+                if M[i-1][j-1]<=min(M[i-1][j]+1,M[i][j-1]+1) and M[i-1][j-1]<=k:
+                   
+                    j=j-1#Moving diagonally
+                    i=i-1
+                elif M[i][j]==M[i-1][j-1]:
+                    
+                    j=j-1#Moving diagonally
+                    i=i-1
+                elif M[i][j-1]+1<=min(M[i-1][j-1],M[i-1][j]+1) and M[i][j-1]<=k:
+                    
+                    j=j-1#Moving horizontally
+                elif M[i-1][j]<=min(M[i-1][j-1],M[i][j-1]+1) and M[i-1][j]<=k:
+                    
+                    i=i-1#Moving vertically
+        else:
+            j=j-1
+ 
+    return list
+
+
+##Uncomment following lines to read from file.
+##T =open("text.fasta","r")
+##
+##P =open("pattern.fasta","r")
+##text=readSeq(T)
+##print "String:",text,"\n"
+##pattern=readSeq(P)
+##print "Pattern",pattern,"\n"
+
+text='ACAGCAG'
+print "String:",text,"\n"
+pattern='GC'
+print "Pattern",pattern,"\n"
+
+n = len(pattern)
+m = len(text)
+
+M=numpy.zeros([n+1,m+1],int)
+
+
+for i in range(0,n+1):
+    M[i,0]=i
+    for j in range(0,m+1):
+        M[0,j]=0
+
+
+text1=list(text)
+pattern1=list(pattern)
+
+
+M = fill(M,text,min, pattern) #Filling matrix M with scores
+print "\n", M
+
+
+#perform traceback
+s= trace(M,pattern,text,1)
+print s
+
+
+
+
---
+++ RNAfoldingi.py
@@ -0,0 +1,106 @@
+##Please install Python 2.7.10 and Numpy 1.10-1.
+##Program for RNA 2-D folding using Nussinov algorithm.
+import numpy
+import array
+class Structure :
+    def __init__(self): 
+        self.paired =[]
+        self.unpaired =[]
+    def merge(self, s):
+        for i in s.paired :
+            self.paired.append(i)
+        for i in s.unpaired :
+            self.unpaired.append(i)
+    def __str__(self):
+        return "paired\n"+str(self.paired) +"\n unpaired \n"+str(self.unpaired)+"\n"
+def l(ch) :
+
+    if (ch=='A') :
+        return 0
+    elif (ch=='C'):
+        return 1
+    elif (ch=='U') :
+        return 2
+    elif (ch =='G') :
+        return 3
+    else :
+        print "error"
+    
+def cost(i,j,M,w,seq,compare): #Defining the cost functions or can say recurrence formula
+    j_unpaired = M[i,j-1]
+    i_unpaired = M[i+1,j]
+    paired = M[i+1,j-1] + w[l(seq[i]), l(seq[j])]
+    
+    return compare(j_unpaired,i_unpaired,paired)  #return comparison of the three relations
+
+def fill(M, w, seq, compare): #function defining how to fill up the values 
+    n = len(seq)
+    for k in range(5,n):
+        for i in range(0,n-k):
+            j = k+i
+            c = cost(i,j,M,w,seq,compare)
+            M[i,j] = c
+
+def trace(M, w, seq, compare, horizontal_arrow, vertical_arrow) :
+    i = horizontal_arrow
+    j = vertical_arrow
+    struct = Structure()
+    while (j != i-1):
+        if M[i,j] == M[i,j-1] :
+            struct.unpaired.append(j)
+            j -= 1
+            
+        elif M[i,j] == M[i+1,j] :
+            
+            struct.unpaired.append(i)
+            
+            i += 1
+        elif M[i,j] == M[i+1,j-1] + w[l(seq[i]), l(seq[j])] :
+            
+            struct.paired.append((i,j))
+            i += 1
+            j -= 1
+        else:
+            print "error"
+
+    return struct
+
+def real_string(struct) : 
+    trace=dict()
+    for element in struct.unpaired:
+        trace[element] = '.'
+
+    for element in struct.paired:
+        trace[element[0]] = '('
+        trace[element[1]] = ')'
+
+    return list(trace.values())
+    
+f =open("example.fasta","r")
+def readSeq(FASTA):
+    for line in FASTA:
+        if line.startswith('>'):
+            continue
+        line = line.strip()
+        return line
+
+    
+seq=readSeq(f)
+
+n = len(seq)
+
+w = numpy.zeros([4,4],int)
+w.fill(0)
+w[l('A'), l('U')] = 1
+w[l('U'), l('A')] = 1 
+w[l('G'), l('C')] = 1
+w[l('C'), l('G')] = 1
+M = numpy.zeros([n,n],int) #initialisation of matrix
+fill(M, w, seq, max) #Filling matrix M with scores
+print M
+
+s = trace(M, w, seq, max, 0, n-1)
+print str(s)
+print seq
+result=real_string(s)
+print "".join(result)
